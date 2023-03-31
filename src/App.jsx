785b13cf35c7941bab817590f78f2273ed19c620
primado <primadoprincepa@gmail.com>
2023-03-31 13:41:50 +0000

    added search input and filter regionn feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import { IoSearchOutline } from "react-icons/io5";
+import { Listbox, Transition } from '@headlessui/react'
+import { IoChevronDownSharp, IoCheckmarkSharp } from "react-icons/io5";
 import useSWR from 'swr'
 
 
 import Navbar from './components/Navbar'
 
+
+const regions = [
+  {name: 'Filter by Region'},
+  {name: 'all'},
+  { name: 'Africa' },
+  { name: 'Americas' },
+  { name: 'Asia' },
+  { name: 'Europe' },
+  { name: 'Oceania' },
+]
+
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const API_URL = 'https://restdata.com/v3.1/all';
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+
 function App() {
+  
+  const [region, setRegion] = useState(regions[1])
+  const [searchTerm, setSearchTerm] = useState("");
+  
 
-  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  // const { data, error } = useSWR(CORS_PROXY + API_URL, fetcher);
 
-  const { data, error } = useSWR(' https://restcountries.com/v3.1/all', fetcher);
+
+
+  const { data, error } = useSWR(
+    `https://restcountries.com/v2/all?fields=name,population,region,capital,flags`,
+    fetcher
+  );
 
   if (error) return <div>Error fetching data</div>;
   if (!data) return <div>Loading...</div>;
+
+  const filteredCountries =
+  region.name === "all"
+    ? data
+    : data.filter((country) => country.region === region.name);
+
+const searchedCountries =
+  searchTerm === ""
+    ? filteredCountries
+    : filteredCountries.filter((country) =>
+        country.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
 
 
   return (
@@ -24,20 +65,81 @@ function App() {
           <form action="#" className='flex justify-between px-20'>
             <div className="form__group w-96">
               
-              <div className="relative">
+              <div className="relative">  
                 <div className="absolute top-0 left-0 mt-3 ml-5">
                   <IoSearchOutline className="text-gray-700 text-xl font-bold" />
                 </div>
                 <input 
                   className="w-full h-12 px-16 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
                   type="text" 
-                  placeholder="Search for a country..." />
+                  placeholder="Search for a country..." 
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                
+                  />
                
               </div>
               
             </div>
-            <div className="form__group">
-              Select a Region
+            <div className=" right-16 w-52 ">
+              <Listbox value={region} onChange={setRegion}>
+                <div className='relative mt-1'>
+                  <Listbox.Button className='relative w-full mb-1 flex items-center bg-white shadow-lg cursor-default rounded-lg py-2 pl-5 h-12 text-left border-2 border-gray-100 focus:outline-none focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-violet-500 focus-visible:ring-offset-2'>
+                    <span className='block truncate'>
+                      {region.name}
+                    </span>
+                    <span className='pointer-events-none absolute inset-y-0  right-0 flex items-center pr-2'>
+                      <IoChevronDownSharp className='w-5 h-5 text-gray-400' aria-hidden="true"/>
+                    </span>
+
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className='absolute shadow-md bg-white'>
+                      {regions.map((region) => (
+                        <Listbox.Option
+                          key={region.name}
+                          disabled={region.name === 'Filter by Region'}
+                          className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active ? 'text-white bg-blue-600' : 'text-gray-900'
+                            }`
+                          }
+
+                          value={region}
+                        >
+                          {({ selected, active}) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? 'font-semibold' : 'font-normal'
+                                }`}
+                              >
+                                {region.name}
+
+                              </span>
+                              { selected ? (
+                                <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-black'>
+                                    <IoCheckmarkSharp 
+                                    className={`w-7 h-7 ${selected ? 'text-amber-500' : 'text-black'}`}
+                                    aria-hidden="true"/>
+                                </span>
+                              ): null }
+                            </>
+                          )}
+
+                        </Listbox.Option>
+                      ))}
+
+                    </Listbox.Options>
+
+                  </Transition>
+
+                </div>
+              </Listbox>
             </div>
           </form>
         </div>
@@ -46,7 +148,7 @@ function App() {
 
           <div className="container">
             <div className="grid grid-cols-4 gap-4 px-20 mt-12">
-              {data.map((country) => (
+              {searchedCountries.map((country) => (
               <div key={country.name.common} className="w-72 max-h-96 mb-12">
                 <img 
                   src={country.flags.svg} 
@@ -55,7 +157,7 @@ function App() {
                 />
                 <div className="description bg-white shadow-lg p-8">
                   <h1 className="font-bold text-xl pb-5">
-                    {country.name.official}
+                    {country.name}
                   </h1>
                   <p> 
                     <span className='font-semibold pr-3'>Population:</span> 
